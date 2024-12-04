9d2da92e850b3a3473497e8c0aedb1b2ade6852f
DarulCode <ahmaddarul72@gmail.com>
2024-12-04 15:15:07 +0700

    feat(init): initialize project structure

---
+++ src/main/java/com/enigma/jobConnector/JobConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.jobConnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/jobConnector/JobConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.jobConnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
